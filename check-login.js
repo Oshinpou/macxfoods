712ecb003399cf3e7acdc8ec0b5ecc2f411b1a3a
Oshinpou <oshinpounikar@gmail.com>
2025-07-07 11:39:17 +0530

    Update check-login.js

--- check-login.js
+++ check-login.js
@@ -1,11 +1,31 @@
 // check-login.js
 
 (function () {
-  const username = localStorage.getItem("macx_loggedInUser");
+  // List of page filenames that require login
+  const protectedPages = [
+    "marketplace.html",
+    "cart.html",
+    "accounts.html",
+    "myorders.html",
+    "index.html",
+    "marketplace.html",
+    "product1.html"
+    "product2.html"
+    
+    // You can add more page filenames here
+  ];
 
-  if (!username) {
-    // Not logged in: save current page and redirect to login
-    localStorage.setItem("macx_returnPage", window.location.href);
-    window.location.href = "login.html";
+  // Extract the current page name from the URL
+  const currentPage = window.location.pathname.split("/").pop();
+
+  // Check if current page is a protected page
+  if (protectedPages.includes(currentPage)) {
+    const username = localStorage.getItem("macx_loggedInUser");
+
+    // If not logged in, redirect to login after saving current page
+    if (!username) {
+      localStorage.setItem("macx_returnPage", window.location.href);
+      window.location.href = "login.html";
+    }
   }
 })();
